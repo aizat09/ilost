eb3cebc144052f095151344b65379470fd769514
Omurbek Kadyrbekov <omurbek.jk@gmail.com>
2017-06-05 05:42:00 -0400

    google api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605084801) do
+ActiveRecord::Schema.define(version: 20170605093216) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,10 +30,12 @@ ActiveRecord::Schema.define(version: 20170605084801) do
   end
 
   create_table "user_providers", force: :cascade do |t|
-    t.string  "uid"
-    t.string  "provider"
-    t.integer "user_id"
-    t.string  "name"
+    t.string   "uid"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
     t.index ["user_id"], name: "index_user_providers_on_user_id"
   end
 
@@ -50,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170605084801) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
